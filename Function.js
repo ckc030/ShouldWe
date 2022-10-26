72d71ccea23517c56555432b490fb6f0a20fbbb1
junkai cao <cjk0638@gmail.com>
2022-10-26 00:42:19 -0400

    day2

--- Function.js
+++ Function.js
@@ -13,3 +13,45 @@ function Disagree(){
     const popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function openMayDetail() {
+    const x = document.getElementById("mayDetail");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+        showSlides(slideIndex)
+
+    }
+}
+
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
